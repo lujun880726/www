9d1699646ff4f8c8b5342d994b73574a9aefeb07
LJ <jun.lu.726@gmail.com>
2014-06-17 10:33:59 +0800

    INIT

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=gb2312" />
+<title>本域名出售 http://www.waiteat.com/ waiteat 等吃</title>
+</head>
+
+<body>
+<p>本域名出售</p>
+<h1>http://www.waiteat.com/</h1>
+<p>有意可联系 QQ：924670 ; email:924670@qq.com/jun.lu.726@gmail.com </p>
+</body>
+<script type="text/javascript">
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F18beaefcfcb16ed17b8be457291079f8' type='text/javascript'%3E%3C/script%3E"));
+</script>
+
+</html>
